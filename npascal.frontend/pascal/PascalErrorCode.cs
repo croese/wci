180bc4cf04549f907eca63c96e78afb6984eb2cf
croese <croese@gmail.com>
2017-10-03 11:37:28 -0500

    Started on error code changes

---
+++ npascal.frontend/pascal/PascalErrorCode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace npascal.frontend.pascal
+{
+  public sealed class ErrorCodeAttribute : Attribute
+  {
+    public string Message { get; }
+    public int Status { get; set; }
+
+    public ErrorCodeAttribute(string message)
+    {
+      Message = message;
+    }
+  }
+
+  public enum PascalErrorCode
+  {
+    [ErrorCode("Already specified in FORWARD")] AlreadyForwarded,
+    [ErrorCode("Object I/O error", Status = -101)] IOError,
+    [ErrorCode("Too many syntax errors", Status = -102)] TooManyErrors
+  }
+
+  public static class ErrorCodeExtensions
+  {
+    public static string GetMessage(this PascalErrorCode errorCode)
+    {
+      return errorCode.GetErrorCodeAttribute().Message;
+    }
+
+    public static int GetStatus(this PascalErrorCode errorCode)
+    {
+      return errorCode.GetErrorCodeAttribute().Status;
+    }
+
+    private static ErrorCodeAttribute GetErrorCodeAttribute(this PascalErrorCode errorCode)
+    {
+      var type = errorCode.GetType();
+      var attribute = type.GetCustomAttribute<ErrorCodeAttribute>();
+      return attribute;
+    }
+  }
+}
--- npascal.frontend/pascal/PascalParserTD.cs
+++ npascal.frontend/pascal/PascalParserTD.cs
@@ -53,17 +53,52 @@ namespace npascal.frontend.pascal
       }
       catch (IOException e)
       {
-        ErrorHandler.AbortTranslation(IO_ERROR, this);
+        ErrorHandler.AbortTranslation(PascalErrorCode.IOError, this);
       }
     }
 
     public override int GetErrorCount()
     {
-      return ErrorHandler.GetErrorCount();
+      return ErrorHandler.ErrorCount;
     }
   }
 
   public class PascalErrorHandler
   {
+    private const int MaxErrors = 25;
+    public int ErrorCount { get; private set; }
+
+    public void Flag(Token token, PascalErrorCode errorCode, Parser parser)
+    {
+      parser.SendMessage(new Message(MessageType.SyntaxError,
+        new object[]
+        {
+          token.LineNumber,
+          token.Position,
+          token.Text,
+          errorCode.ToString()
+        }));
+
+      ErrorCount++;
+      if (ErrorCount > MaxErrors)
+      {
+        AbortTranslation(PascalErrorCode.TooManyErrors, parser);
+      }
+    }
+
+    public void AbortTranslation(PascalErrorCode errorCode, Parser parser)
+    {
+      var fatalText = $"FATAL ERROR: " + errorCode.ToString();
+      parser.SendMessage(new Message(MessageType.SyntaxError,
+        new object[]
+        {
+          0,
+          0,
+          "",
+          fatalText
+        }));
+
+      Environment.Exit(errorCode.GetStatus());
+    }
   }
 }
